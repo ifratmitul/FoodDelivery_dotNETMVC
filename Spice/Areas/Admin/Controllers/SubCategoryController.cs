02b5dfb15aaab418b014e2d1caeecaa9da3c0a6a
ifrat Mitul <ifrat.mitul@hotmail.com>
2020-09-03 10:59:54 +0600

    viewmodels, Extensions for Sub-Cat added

--- Spice/Areas/Admin/Controllers/SubCategoryController.cs
+++ Spice/Areas/Admin/Controllers/SubCategoryController.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.EntityFrameworkCore;
 using Spice.Data;
+using Spice.Models;
+using Spice.Models.ViewModels;
 
 namespace Spice.Areas.Admin.Controllers
 {
@@ -22,6 +25,22 @@ namespace Spice.Areas.Admin.Controllers
         {
             var subcategory = await _db.SubCategory.Include(s => s.Category).ToListAsync();
             return View(subcategory);
+        }
+
+        //GET create
+
+        public async Task<IActionResult> Create()
+        {
+            SubCategoryAndCategoryViewModel model = new SubCategoryAndCategoryViewModel()
+            {
+                CategoryList = await _db.Catagory.ToListAsync(),
+                SubCategory = new Models.SubCategory(),
+                subCategoryList = await _db.SubCategory.OrderBy(p => p.Name).Select(p => p.Name).Distinct().ToListAsync()
+
+
+            };
+            return View(model);
+        
         }
     }
 }
---
+++ Spice/Models/ViewModels/SubCategoryAndCategoryViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spice.Models.ViewModels
+{
+    public class SubCategoryAndCategoryViewModel
+    {
+        public IEnumerable<Catagory> CategoryList { get; set; }
+        public SubCategory SubCategory { get; set; }
+        public  List<string> subCategoryList  { get; set; }
+        public string StatusMessage { get; set; }
+
+    }
+}
